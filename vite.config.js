aa809afbf8808c897e8b0ab570f623bfcb047a65
umair <umairkh8251@gmail.com>
2024-09-29 23:28:44 +0500

    Github Pages configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/Hosterr-TailwindCSS/', // Change this to your repo name
+  plugins: [react()]
+});
